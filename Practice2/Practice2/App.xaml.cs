fbeab77a319b1c592c1c29aa237f189220885c2f
Serchs <sercont_01@hotmail.com>
2017-09-11 13:26:05 -0500

    test

---
+++ Practice2/Practice2/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Practice2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //InitializeComponent();
+
+            //MainPage = new Practice2.MainPage();
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "WELCOME TO XAMARIN FORMS !"
+                        }
+                    }
+                }
+            };
+
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
